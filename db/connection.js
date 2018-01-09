82895bb5062dca987906e150e248b1df2ea9723b
Shelley Schoppert <schop23@gmail.com>
2018-01-09 15:48:27 -0500

    updates

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/kryptonite', {
-  useMongoClient: true
-})
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect("mongodb://localhost/kryptonite");
+}
 
 mongoose.Promise = Promise
 
